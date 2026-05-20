78073b8a0c821daeb0ebee982f8667b0c07b5aaa
Emerson <emedzh@gmail.com>
2026-05-20 09:06:46 -0600

    feat(auth): implement confirm screen ui

--- app/src/main/java/com/example/sneakers_admin_app/features/auth/presentation/screens/ConfirmScreen.kt
+++ app/src/main/java/com/example/sneakers_admin_app/features/auth/presentation/screens/ConfirmScreen.kt
@@ -1,19 +1,43 @@
 package com.example.sneakers_admin_app.features.auth.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.sneakers_admin_app.navigation.Routes
+import com.example.sneakers_admin_app.shared.components.PrimaryButton
 
 @Composable
 fun ConfirmScreen(navController: NavController) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 18.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text("Confirm")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text("Account created successfully", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+            Text("Now you can log in", fontSize = 14.sp)
+            PrimaryButton(modifier = Modifier.fillMaxWidth(), text = "Log in", onClick = {
+                navController.navigate(Routes.LOGIN) {
+                    popUpTo(Routes.CONFIRM) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
     }
 }
