1e8258186c791ae84b16d4522112567788b61eb8
Emerson <emedzh@gmail.com>
2026-05-20 11:19:52 -0600

    feat(auth): implement login screen ui

--- app/src/main/java/com/example/sneakers_admin_app/features/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/sneakers_admin_app/features/auth/login/LoginScreen.kt
@@ -1,19 +1,68 @@
 package com.example.sneakers_admin_app.features.auth.login
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.sneakers_admin_app.navigation.Routes
+import com.example.sneakers_admin_app.shared.components.PrimaryButton
+import com.example.sneakers_admin_app.ui.theme.AppColors
 
 @Composable
-fun LoginScreen(navController: NavController) {
+fun LoginScreen(
+    viewModel: LoginViewModel = viewModel(),
+    navController: NavController
+) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 18.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text("Login")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Log in", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = viewModel.email,
+                onValueChange = { viewModel.onEmailChange(it) },
+                placeholder = { Text("Email", color = AppColors.TextMuted) }
+            )
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = viewModel.password,
+                onValueChange = { viewModel.onPasswordChange(it) },
+                placeholder = { Text("Password", color = AppColors.TextMuted) }
+            )
+
+            PrimaryButton(modifier = Modifier.fillMaxWidth(), text = "Log in", onClick = {})
+
+            Text(modifier = Modifier.clickable {
+                navController.navigate(Routes.REGISTER) {
+                    popUpTo(Routes.REGISTER) {
+                        inclusive = false
+                    }
+
+                    launchSingleTop = true
+                } }, text = "¿Don't have an account? Register", fontSize = 12.sp)
+        }
     }
 }
---
+++ app/src/main/java/com/example/sneakers_admin_app/features/auth/login/LoginViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.sneakers_admin_app.features.auth.login
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    var email by mutableStateOf("")
+        private set
+    var password by mutableStateOf("")
+        private set
+
+    fun onEmailChange(value: String) {
+        email = value
+    }
+
+    fun onPasswordChange(value: String) {
+        password = value
+    }
+}
