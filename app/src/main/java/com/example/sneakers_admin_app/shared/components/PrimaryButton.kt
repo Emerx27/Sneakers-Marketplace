36fc7add494476ae62e1ca3e1ac3a1d1690e566b
Emerson <emedzh@gmail.com>
2026-05-19 18:12:13 -0600

    refactor: improve reusable button component

--- app/src/main/java/com/example/sneakers_admin_app/shared/components/PrimaryButton.kt
+++ app/src/main/java/com/example/sneakers_admin_app/shared/components/PrimaryButton.kt
@@ -2,18 +2,22 @@ package com.example.sneakers_admin_app.shared.components
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PrimaryButton(
     modifier: Modifier = Modifier,
     text: String,
+    containerColor: Color = MaterialTheme.colorScheme.primary,
     onClick: () -> Unit
 ) {
-    Button(modifier = modifier, onClick = onClick, shape = RoundedCornerShape(4.dp)) {
+    Button(modifier = modifier, colors = ButtonDefaults.buttonColors(containerColor = containerColor), onClick = onClick, shape = RoundedCornerShape(4.dp)) {
         Text(text)
     }
 }
