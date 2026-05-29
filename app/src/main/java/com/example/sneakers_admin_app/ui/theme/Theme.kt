dc2cfd3831b11146f2f97cdcce0df0d8ff1af4d5
Emerson <emedzh@gmail.com>
2026-05-29 10:25:36 -0600

    style: improve app color palette

--- app/src/main/java/com/example/sneakers_admin_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/sneakers_admin_app/ui/theme/Theme.kt
@@ -1,14 +1,10 @@
 package com.example.sneakers_admin_app.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     background = Gray900,
@@ -41,8 +37,6 @@ object AppColors {
 @Composable
 fun Products_AdminTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
